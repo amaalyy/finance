50c8a019bddaae4976fbc659e39ebf0f08ef329f
Sgun369 <souhaiebjebali@gmail.com>
2024-02-15 12:44:02 +0100

    add CORS_ALLOW_ALL_ORIGINS = True to settings.py

--- finance_app/settings.py
+++ finance_app/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
 
     'corsheaders.midleware.CorsMiddleware',
-    
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -139,3 +139,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOW_ALL_ORIGINS = True
