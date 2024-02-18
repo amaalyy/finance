2eba07d7e331513982e869dcf3acb9216db4303d
Sgun369 <souhaiebjebali@gmail.com>
2024-02-18 13:55:07 +0100

    adding some pages and handle tokens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,27 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Login from './pages/Login'; // Import from the pages folder
+import RegisterPage from './pages/RegisterPage' // Import the RegisterPage from the pages folder
+import HomePage from './pages/HomePage'
+
+import { AuthProvider } from './contexts/AuthContext'
 
 
 function App() {
   return (
+    <AuthProvider>
+    <Router>
     <div className="App">
       <h1>My App</h1>
-      <Login />
+      <Routes>
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/home" element ={<HomePage />} />
+      <Route path="/" element={<Login />} />
+      </Routes>
     </div>
+    </Router>
+    </AuthProvider>
   );
 }
 
---
+++ frontend/src/contexts/AuthContext.js
@@ -0,0 +1,66 @@
+import React, { createContext, useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    
+  // State to manage the user data
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  
+
+  // Function to handle user login
+  const login = (userData) => {
+    setUser(userData);
+    localStorage.setItem('token', userData.token)
+  };
+
+  // Function to handle user logout
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem('token');
+    
+  };
+
+  // Function to check if the user is authenticated (e.g., on page load)
+  const checkAuth = async () => {
+    try {
+      const token = localStorage.getItem('token');
+
+      if (token) {
+        const response = await axios.get('http://127.0.0.1:8000/api/check-auth', {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        setUser(response.data);
+      }
+    } catch (error) {
+      console.error('Authentication check failed:', error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // Run checkAuth on component mount
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
+  // The context value that will be provided to components
+  const contextValue = {
+    user,
+    login,
+    logout,
+    checkAuth,
+  };
+
+  return (
+    <AuthContext.Provider value={contextValue}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
