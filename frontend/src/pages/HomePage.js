879dd02eb0a35e6fd2d1cbeaa2de2197b80315a9
Sgun369 <souhaiebjebali@gmail.com>
2024-02-19 17:17:47 +0100

    .\HomePage.js

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,14 +1,41 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import TransactionList from '../components/TransactionList'
+import TransactionActions from '../components/TransactionActions'
+import axios from 'axios'
+
+
 
 const HomePage = () => {
     const { user, logout, loading } = useContext(AuthContext);
+    const [transactions, setTransactions] = useState([]);
     console.log('user in HomePage');
     if (loading) {
       return <p>Loading...</p>
+    }
+    const token = localStorage.getItem('token');
+    const axiosInstance = axios.create({
+      baseURL: 'http://127.0.0.1:8000/api/',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'content-type': 'application/json',
+      }
+    })
+
+    const handleAddTransaction = async (newTransaction) => {
+      try {
+        console.log('Adding trransaction:', newTransaction);
+        const response = await axiosInstance.post('/transaction/',newTransaction);
+        console.log('Transaction added successfully:', response.data);
+        setTransactions([...transactions, response.data]);
+      } catch (error) {
+        console.error('Error adding transaction:', error);
+        if (error.response) {
+          console.error('Response data:', error.response.data);
+        }
+      }
     }
     return(
         <div>
@@ -17,6 +44,7 @@ const HomePage = () => {
         {user ? (
           <div>
             <p>Hello, you are logged in as {user.username}!</p>
+            <TransactionActions onAddTransaction={handleAddTransaction} />
             <TransactionList />
             
             <button onClick={logout}>Logout</button>
