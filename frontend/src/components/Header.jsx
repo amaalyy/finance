6a874f711126c8bee8418a302170f5d4a3bbbe53
maouia <maouiabend@gmail.com>
2024-03-02 23:06:42 +0100

    fix logout

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,25 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate} from 'react-router-dom';
+
+import { AuthContext } from '../contexts/AuthContext';
+import { useContext, useEffect } from 'react';
+
+
 
 const Header = () => {
+  const { logout, user } = useContext(AuthContext);
+  const navigate = useNavigate();
+    useEffect(() => {
+      if (!user) {
+        navigate('/login');
+      }
+    }, []);
   return (
     <header>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <Link className="navbar-brand" to="/">
           Wealthwise
         </Link>
-        <div className="collapse navbar-collapse" id="navbarNav">
+        <div className=" navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
               <Link className="nav-link" to="/home">
@@ -23,6 +35,9 @@ const Header = () => {
               <Link className="nav-link" to="/signup">
                 Signup
               </Link>
+            </li>
+            <li>
+            <button onClick={logout}>Logout</button>
             </li>
           </ul>
         </div>
