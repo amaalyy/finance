585ee05d20a953b5dae560b6c66b1d3df6eef44c
maouia <maouiabend@gmail.com>
2024-03-02 23:05:47 +0100

    fix routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,22 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Login from './pages/Login'; // Import from the pages folder
-import Signup from './pages/Signup' // Import the Sign up from the pages folder
-import HomePage from './pages/HomePage'
-
-import { AuthProvider } from './contexts/AuthContext'
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import HomePage from './pages/HomePage';
+import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
   return (
     
     <Router>
-    <div className="App">
-    
-      <Routes>
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/home" element ={<HomePage />} />
-      <Route path="/" element={<Login />} />
-      </Routes>
-    </div>
+      <div className="App">
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/" element={<HomePage />} /> {/* Assign HomePage to "/" */}
+        </Routes>
+      </div>
     </Router>
     
   );
