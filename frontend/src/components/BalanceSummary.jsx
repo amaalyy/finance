c8d6624872163c18c0f5ddfb6b646cf03cdbe473
Sgun369 <souhaiebjebali@gmail.com>
2024-03-15 14:01:47 +0100

    just playing around

--- frontend/src/components/BalanceSummary.jsx
+++ frontend/src/components/BalanceSummary.jsx
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Card from './Card';
+import { Bar } from 'react-chartjs-2';
 
 const BalanceSummary = (forceRemount) => {
   const [balanceData, setBalanceData] = useState(null);
@@ -25,15 +26,55 @@ const BalanceSummary = (forceRemount) => {
     fetchBalance();
   }, [forceRemount]);
 
+  const chartData = {
+    labels: ['Total Income', 'Total Expense', 'Balance'],
+    datasets: [
+      {
+        label: 'TND', // Tunisian Dinar
+        backgroundColor: [
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+        ],
+        borderColor: [
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(75, 192, 192, 1)',
+        ],
+        borderWidth: 1,
+        data: [
+          balanceData ? balanceData.total_income : 0,
+          balanceData ? balanceData.total_expense : 0,
+          balanceData ? balanceData.balance : 0,
+        ],
+      },
+    ],
+  };
+const chartOptions = {
+  scales: {
+    yAxes: [
+      {
+        ticks: {
+          beginAtZero: true,
+        },
+      },
+    ],
+  },
+};
+    
+
   return (
     <Card title="Balance Summary">
       {error && <p>Error fetching balance: {error}</p>}
       {balanceData && (
-        <div>
+        <><div>
           <p>Total Income: TND {balanceData.total_income}</p>
           <p>Total Expense: TND {balanceData.total_expense}</p>
           <p>Balance: TND {balanceData.balance}</p>
         </div>
+        <div className="h-[600px] w-[600px] bg-white">
+            <Bar data={chartData} options={chartOptions} />
+          </div></>
       )}
     </Card>
   );
