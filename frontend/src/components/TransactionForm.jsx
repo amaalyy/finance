898f7cd1efd686cf9a4e198a390e701adae64d52
Sgun369 <souhaiebjebali@gmail.com>
2024-03-17 22:52:51 +0100

    TransactionForm refined

--- frontend/src/components/TransactionForm.jsx
+++ frontend/src/components/TransactionForm.jsx
@@ -1,11 +1,11 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const TransactionForm = ({ onAddTransaction }) => {
   const [description, setDescription] = useState('');
   const [amount, setAmount] = useState('');
   const [category, setCategory] = useState('');
-  const [transactionType, setTransactionType] = useState(''); // Assuming transactionType is a state variable
+  const [transactionType, setTransactionType] = useState('');
   const [categories, setCategories] = useState([]);
   const [error, setError] = useState(null);
 
@@ -21,7 +21,6 @@ const TransactionForm = ({ onAddTransaction }) => {
           Authorization: `Bearer ${token}`,
         },
       });
-      console.log(response);
       if (response.status !== 200) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
@@ -47,7 +46,7 @@ const TransactionForm = ({ onAddTransaction }) => {
       description,
       amount: transactionAmount,
       category,
-      transaction_type: transactionType, // corrected variable name
+      transaction_type: transactionType,
     };
 
     // Call the callback function from the parent component
@@ -57,48 +56,77 @@ const TransactionForm = ({ onAddTransaction }) => {
     setDescription('');
     setAmount('');
     setCategory('');
-    setTransactionType('EX'); // Clear transaction_type if needed
+    setTransactionType('EX');
   };
 
   return (
-    <div>
-      <h3>Add New Transaction</h3>
-      <label>
-        Description:
-        <input
-          type="text"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
-      </label>
-      <label>
-        Amount:
-        <input
-          type="number"
-          value={amount}
-          onChange={(e) => setAmount(e.target.value)}
-        />
-      </label>
-      <label>
-        Category:
-        <select value={category} onChange={(e) => setCategory(e.target.value)}>
-          <option value="">---------------</option>
-          {categories.map((category) => (
-            <option key={category.id} value={category.id}>
-              {category.name}
-            </option>
-          ))}
-        </select>
-      </label>
-      <label>
-        Transaction Type:
-        <select value={transactionType} onChange={(e) => setTransactionType(e.target.value)}>
-          <option value="">----------</option>
-          <option value="EX">Expense </option>
-          <option value="IN">Income </option>
-        </select>
-      </label>
-      <button onClick={handleAddTransaction}>Add Transaction</button>
+    <div className="max-w-md mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <h3 className="text-xl font-semibold mb-4">Add New Transaction</h3>
+      <div className="mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="description">
+          Description:
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="description"
+            type="text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </label>
+      </div>
+      <div className="mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="amount">
+          Amount:
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="amount"
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+        </label>
+      </div>
+      <div className="mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="category">
+          Category:
+          <select
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="category"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option value="">---------------</option>
+            {categories.map((category) => (
+              <option key={category.id} value={category.id}>
+                {category.name}
+              </option>
+            ))}
+          </select>
+        </label>
+      </div>
+      <div className="mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="transactionType">
+          Transaction Type:
+          <select
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="transactionType"
+            value={transactionType}
+            onChange={(e) => setTransactionType(e.target.value)}
+          >
+            <option value="">----------</option>
+            <option value="EX">Expense</option>
+            <option value="IN">Income</option>
+          </select>
+        </label>
+      </div>
+      <div className="bg-blue-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+      <button
+        
+        onClick={handleAddTransaction}
+      >
+        Add Transaction
+      </button>
+      </div>
     </div>
   );
 };
