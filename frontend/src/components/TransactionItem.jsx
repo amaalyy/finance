e339d338c92bad40076e8fb1a22a868c22e078dc
amaalyy <amal.elzowawi@gmail.com>
2024-03-18 04:03:36 +0100

    adding things

--- frontend/src/components/TransactionItem.jsx
+++ frontend/src/components/TransactionItem.jsx
@@ -11,24 +11,24 @@ const TransactionItem = ({
   const transactionColor = transactionType === "IN" ? "text-green-500" : "text-red-500";
 
   return (
-    <div className="m-4 w-2/3 border border-gray-300 p-4 rounded flex flex-col relative">
+    <div className="mx-8 my-8 grid grid-rows-[1fr_1fr_1fr_1fr] bg-white p-4 rounded-3xl drop-shadow-xl w-[400px] antialiased">
       <div className="mb-4">
         {/* <span className="text-lg mr-2">ID: {id}</span> */}
-        <span className="text-lg">Transaction Type: {transactionType === "IN" ? "Income" : "Expense"}</span>
+        <span className="text-lg font-medium">Transaction Type: {transactionType === "IN" ? "Income" : "Expense"}</span>
       </div>
       <div className="mb-4">
-        <span>Amount:</span>
+        <span className="text-lg font-medium">Amount:</span>
         <span className={`ml-2 ${transactionColor}`}>TND{amount}</span>
       </div>
       <div className="mb-4">
-        <span>Category: {category}</span>
+        <span className="text-lg font-medium">Category: {category}</span>
       </div>
       <div className="mb-4">
-        <span>Description: {description}</span>
+        <span className="text-lg font-medium">Description: {description}</span>
       </div>
-      <div className="absolute right-0 top-1/2 transform -translate-y-1/2">
+      <div className="right-0 top-1/2 transform -translate-y-1/2">
         <button
-          className="bg-red-500 text-white px-4 py-2 border border-red-600 rounded cursor-pointer mr-12"
+          className="bg-red-500 text-white px-4 py-2 border border-red-600 rounded cursor-pointer"
           onClick={() => onDeleteClick(id)}
         >
           Delete
