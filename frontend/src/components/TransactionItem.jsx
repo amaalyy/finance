60b4cf0d58301b60ca0356b7f8e0f22f424b83da
amaalyy <amal.elzowawi@gmail.com>
2024-03-19 00:19:40 +0100

    add things

--- frontend/src/components/TransactionItem.jsx
+++ frontend/src/components/TransactionItem.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 const TransactionItem = ({
   id,
@@ -6,27 +6,32 @@ const TransactionItem = ({
   amount,
   category,
   description,
-  onDeleteClick,
+  onDeleteClick
 }) => {
-  const transactionColor = transactionType === "IN" ? "text-green-500" : "text-red-500";
+  const transactionColor =
+    transactionType === 'IN' ? 'text-green-500' : 'text-red-500';
 
   return (
     <div className="mx-8 my-8 grid grid-rows-[1fr_1fr_1fr_1fr] bg-white p-4 rounded-3xl drop-shadow-xl w-[400px] antialiased">
-      <div className="mb-4">
+      <div className="mb-2">
         {/* <span className="text-lg mr-2">ID: {id}</span> */}
-        <span className="text-lg font-medium">Transaction Type: {transactionType === "IN" ? "Income" : "Expense"}</span>
+        <span className="text-lg font-medium px-4">
+          Transaction Type: {transactionType === 'IN' ? 'Income' : 'Expense'}
+        </span>
       </div>
-      <div className="mb-4">
-        <span className="text-lg font-medium">Amount:</span>
-        <span className={`ml-2 ${transactionColor}`}>TND{amount}</span>
+      <div className="">
+        <span className="text-lg font-medium pl-4">Amount:</span>
+        <span className={` ${transactionColor}`}> {amount}TND</span>
       </div>
-      <div className="mb-4">
-        <span className="text-lg font-medium">Category: {category}</span>
+      <div className="">
+        <span className="text-lg font-medium px-4">Category: {category}</span>
       </div>
-      <div className="mb-4">
-        <span className="text-lg font-medium">Description: {description}</span>
+      <div className="">
+        <span className="text-lg font-medium px-4">
+          Description: {description}
+        </span>
       </div>
-      <div className="right-0 top-1/2 transform -translate-y-1/2">
+      <div className="px-4">
         <button
           className="bg-red-500 text-white px-4 py-2 border border-red-600 rounded cursor-pointer"
           onClick={() => onDeleteClick(id)}
